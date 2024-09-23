9a9645f192550556b3b3ab6f8023fa928a46755e
Cris <crisvera1909@gmail.com>
2024-09-22 21:12:18 -0500

    bottom sheet acomodado para poner la tabla

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const resizeBtn = document.querySelector("[data-resize-btn]");
+const $ = document.querySelector.bind(document);
+
+const resizeBtn = $("[data-resize-btn]");
 
 resizeBtn.addEventListener("click", function (e) {
   e.preventDefault();
