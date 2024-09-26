ea46447663aae2d7ae0051c75a061d96d4e44cb8
Cris <crisvera1909@gmail.com>
2024-09-26 08:32:10 -0500

    guardando cambios

--- scripts/incrase-logic.js
+++ scripts/incrase-logic.js
@@ -1,15 +1,15 @@
-const quantitySpan = document.getElementById("quantity");
-const decreaseButton = document.getElementById("decrease");
-const increaseButton = document.getElementById("increase");
+const quantitySpan = document.querySelector("#quantity");
+const decreaseButton = document.querySelector("#decrease");
+const increaseButton = document.querySelector("#increase");
 
 decreaseButton.addEventListener("click", () => {
-  let currentValue = parseInt(quantitySpan.textContent);
+  const currentValue = parseInt(quantitySpan.textContent);
   if (currentValue > 1) {
     quantitySpan.innerHTML = currentValue - 1;
   }
 });
 
 increaseButton.addEventListener("click", () => {
-  let currentValue = parseInt(quantitySpan.textContent);
+  const currentValue = parseInt(quantitySpan.textContent);
   quantitySpan.innerHTML = currentValue + 1;
 });
